c283ce3554c5127f4b9dc6c2dc330c96db707c18
mazzac409 <mazzachris.work@gmail.com>
2018-12-29 15:35:30 -0600

    Initial Project Add

---
+++ src/main/java/mine/mazzac409/wishlist/WishListApplication.java
@@ -0,0 +1,14 @@
+package mine.mazzac409.wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishListApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/mine/mazzac409/wishlist/WishListApplicationTests.java
@@ -0,0 +1,17 @@
+package mine.mazzac409.wishlist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WishListApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
